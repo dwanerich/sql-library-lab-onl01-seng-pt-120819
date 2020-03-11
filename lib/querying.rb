4b77c6a4dcdc38b5cb58b9b84383ce01e5d1d826
dwane richards <dwanerich@gmail.com>
2020-03-11 22:52:20 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,14 +53,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series.title AS title, COUNT(characters.series_id) AS num_species
-      FROM series
-      INNER JOIN characters
-        ON series.id = characters.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
-      ORDER BY num_species DESC
-      LIMIT 1);"
+  "SELECT series.title
+  FROM series
+  JOIN books
+  ON series.id = books.series_id
+  JOIN character_books
+  ON books.id = character_books.book_id JOIN characters ON
+  character_books.character_id = characters.id
+  GROUP BY characters.species, series.title HAVING characters.species = 'human' LIMIT 1";
 end
 
 def select_character_names_and_number_of_books_they_are_in
