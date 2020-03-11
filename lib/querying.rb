4f074fbade7ee8e5b28320a7d76e4b9699ceae7e
dwane richards <dwanerich@gmail.com>
2020-03-11 22:12:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,7 +53,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series.title AS title, COUNT(characters.series_id) AS num_species
+  "SELECT title FROM (
+    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
       FROM series
       INNER JOIN characters
         ON series.id = characters.series_id
