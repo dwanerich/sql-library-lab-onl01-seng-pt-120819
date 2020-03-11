ea0c48548083c9c430aa5c1d4596299c270c1ea7
dwane richards <dwanerich@gmail.com>
2020-03-11 22:31:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -59,7 +59,7 @@ def select_series_title_with_most_human_characters
         ON series.id = characters.series_id
       WHERE characters.species = 'human'
       GROUP BY series.title
-      ORDER BY COUNT(characters.species) DESC
+      ORDER BY COUNT(characters.species = 'human') = DESC
       LIMIT 1;"
 end
 
