b3572904782d085a2840e2507c3b1699724e3723
dwane richards <dwanerich@gmail.com>
2020-03-11 22:51:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -58,7 +58,9 @@ def select_series_title_with_most_human_characters
   JOIN books
   ON series.id = books.series_id
   JOIN character_books
-  ON BOOKS.
+  ON books.id = character_books.book_id JOIN characters ON
+  character_books.characeter_id = characeters.id
+  GROUP BY characters.species, series.title HAVING characters.species = 'human' LIMIT 1";
 end
 
 def select_character_names_and_number_of_books_they_are_in
